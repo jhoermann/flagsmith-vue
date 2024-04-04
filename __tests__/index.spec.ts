72e82579746e873b318ef6d9012a0d555f97af84
Jochen Hörmann <jochen.hoermann@gmail.com>
2024-04-04 18:24:43 +0200

    test: add tests for feature flags

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,12 +1,25 @@
-import { defineComponent } from 'vue'
-import { useFlagsmith } from '../src/index'
+import { computed, defineComponent, nextTick } from 'vue'
+import { useFlags, useFlagsmith } from '../src/index'
 import { mount } from '@vue/test-utils'
+import flagsmith from 'flagsmith'
 
 jest.mock('flagsmith')
 
 const ChildComponent = defineComponent({
-    template: '<div></div>',
+    template: '<div v-if="isVisible" class="child-component"></div>',
+    setup() {
+        const { is_visible: isVisibleFeature } = useFlags(['is_visible'])
+        const isVisible = computed(() => isVisibleFeature.value?.value)
+
+        return { isVisible }
+    },
 })
+
+const isVisibleFeatureMock = {
+    id: 1,
+    enabled: true,
+    value: true,
+}
 
 const ParentComponent = defineComponent({
     template: '<main><child-component /></main>',
@@ -22,8 +35,32 @@ const mountParentComponent = () => mount(ParentComponent)
 
 describe('flagsmith-vue', () => {
     describe('Flags', () => {
-        mountParentComponent()
-        test.todo('should have feature flags')
+        it('should show the child component when feature flag is set to true', async () => {
+            jest.mocked(flagsmith.getAllFlags).mockImplementation(() => ({
+                is_visible: isVisibleFeatureMock,
+            }))
+            const wrapper = mountParentComponent()
+
+            flagsmith._trigger!()
+            await nextTick()
+
+            expect(() => wrapper.get('.child-component')).not.toThrow()
+        })
+
+        it('should not show the child component when feature flag is set false', async () => {
+            jest.mocked(flagsmith.getAllFlags).mockImplementation(() => ({
+                is_visible: {
+                    ...isVisibleFeatureMock,
+                    value: false,
+                },
+            }))
+            const wrapper = mountParentComponent()
+
+            flagsmith._trigger!()
+            await nextTick()
+
+            expect(() => wrapper.get('.child-component')).toThrow()
+        })
     })
 
     describe('Traits', () => {
