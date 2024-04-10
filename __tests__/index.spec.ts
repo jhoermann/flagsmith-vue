7671be4ddeafa7e87c32f96d70478308c64a4ee6
Jochen Hörmann <jochen.hoermann@gmail.com>
2024-04-10 14:36:29 +0200

    test: add tests for traits and loading status

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,17 +1,28 @@
 import { computed, defineComponent, nextTick } from 'vue'
-import { useFlags, useFlagsmith } from '../src/index'
+import { useFlags, useFlagsmith, useFlagsmithLoading, useTraits } from '../src/index'
 import { mount } from '@vue/test-utils'
 import flagsmith from 'flagsmith'
 
 jest.mock('flagsmith')
 
 const ChildComponent = defineComponent({
-    template: '<div v-if="isVisible" class="child-component"></div>',
+    template: `
+    <div class="child-component"
+        :test-trait="testTrait">
+        <div v-if="isVisible"
+            class="child-component--feature">
+        </div>
+        <div v-if="isLoading"
+            class="child-component--loading">
+        </div>
+    </div>`,
     setup() {
         const { is_visible: isVisibleFeature } = useFlags(['is_visible'])
         const isVisible = computed(() => isVisibleFeature.value?.value)
+        const { test_trait: testTrait } = useTraits(['test_trait'])
+        const { isLoading } = useFlagsmithLoading()
 
-        return { isVisible }
+        return { isVisible, testTrait, isLoading }
     },
 })
 
@@ -22,7 +33,7 @@ const isVisibleFeatureMock = {
 }
 
 const ParentComponent = defineComponent({
-    template: '<main><child-component /></main>',
+    template: `<main><child-component /></main>`,
     components: { ChildComponent },
     setup() {
         useFlagsmith({
@@ -44,7 +55,7 @@ describe('flagsmith-vue', () => {
             flagsmith._trigger!()
             await nextTick()
 
-            expect(() => wrapper.get('.child-component')).not.toThrow()
+            expect(() => wrapper.get('.child-component--feature')).not.toThrow()
         })
 
         it('should not show the child component when feature flag is set false', async () => {
@@ -59,15 +70,53 @@ describe('flagsmith-vue', () => {
             flagsmith._trigger!()
             await nextTick()
 
-            expect(() => wrapper.get('.child-component')).toThrow()
+            expect(() => wrapper.get('.child-component--feature')).toThrow()
         })
     })
 
     describe('Traits', () => {
-        test.todo('should have traits')
+        it('should display the trait attribute when the trait is set', async () => {
+            jest.mocked(flagsmith.getAllTraits).mockImplementation(() => ({
+                test_trait: 'test-trait',
+            }))
+            const wrapper = mountParentComponent()
+
+            flagsmith._trigger!()
+            await nextTick()
+
+            expect(wrapper.get('.child-component').attributes('test-trait')).toBe('test-trait')
+        })
+
+        it('should not display the trait attribute when the trait is not set', async () => {
+            jest.mocked(flagsmith.getAllTraits).mockImplementation(() => ({}))
+            const wrapper = mountParentComponent()
+
+            flagsmith._trigger!()
+            await nextTick()
+
+            expect(wrapper.get('.child-component').attributes('test-trait')).toBeUndefined()
+        })
     })
 
     describe('Loading status', () => {
-        test.todo('should have current loading status')
+        it('should display the loading element when flagsmith is loading', async () => {
+            jest.mocked(flagsmith.loadingState!).isLoading = true
+            const wrapper = mountParentComponent()
+
+            flagsmith._triggerLoadingState!()
+            await nextTick()
+
+            expect(() => wrapper.get('.child-component--loading')).not.toThrow()
+        })
+
+        it('should not display the loading element when flagsmith is not loading', async () => {
+            jest.mocked(flagsmith.loadingState!).isLoading = false
+            const wrapper = mountParentComponent()
+
+            flagsmith._triggerLoadingState!()
+            await nextTick()
+
+            expect(() => wrapper.get('.child-component--loading')).toThrow()
+        })
     })
 })
